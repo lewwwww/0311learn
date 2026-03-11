5df961d9a0e01a0cf59c0efa86b80f10ed090fb4
lewwwww <fengsheng_2020@qq.com>
2026-03-11 22:25:35 +0800

    添加 MallChat-main 项目文件夹，包含工具类模块

---
+++ xiangmu/MallChat-main/MallChat-main/mallchat-chat-server/src/main/java/com/abin/mallchat/common/MallchatCustomApplication.java
@@ -0,0 +1,22 @@
+package com.abin.mallchat.common;
+
+import org.apache.rocketmq.spring.autoconfigure.RocketMQAutoConfiguration;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+/**
+ * @author zhongzb
+ * @date 2021/05/27
+ */
+@SpringBootApplication(scanBasePackages = {"com.abin.mallchat"},exclude={RocketMQAutoConfiguration.class})
+@MapperScan({"com.abin.mallchat.common.**.mapper"})
+@ServletComponentScan
+public class MallchatCustomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallchatCustomApplication.class,args);
+    }
+
+}
